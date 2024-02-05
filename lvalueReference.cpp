be1753fdbd905c0fdd168f0253722438760ebfa4
seherkavsur <seherkavsur21@gmail.com>
2024-02-05 14:51:25 +0300

    l value referance

---
+++ lvalueReference.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+void func(int t){
+    ++t;
+    
+}
+void funcref(int &k){
+    ++k;
+}
+int main(){
+    int value = 10;
+    func(value);
+    std::cout  <<"the value of x after the func() is called   :" << value<< std::endl;
+    funcref(value);
+    std::cout << "the value of x after the funcref() is called:"<<value<< std::endl;
+     
+}
